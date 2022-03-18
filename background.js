7ca39a3591e150a4eaf05bbc27a42f66ad164293
nhnam <37695601+hoai97nam@users.noreply.github.com>
2022-03-18 21:37:59 +0700

    Update background.js
    test amend commit

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
-var s = 0;
-var r = 0;
-var l = 0;
-var n = 0;
-var o = 0;
-var c = 0;
-var u = false;
-var i = false;
-var m = "";
-var g = {};
-var e = {
+var bien1 = 0;
+var bien2 = 0;
+var bien3 = 0;
+var bien4 = 0;
+var bien5 = 0;
+var bien6 = 0;
+var bien7 = false;
+var bien8 = false;
+var bien9 = "";
+var bien10 = {};
+var bien11 = {
     current: 0,
     total: 0
 };
@@ -80,17 +80,17 @@ var usrs = {
         'expireDate': ""
     }
 };
-chrome.runtime.onMessage.addListener(function (e, t, a) {
-    if (e.message == "activate_icon") chrome.pageAction.show(t.tab.id);
-    if (e.message == "pause") i = true;
-    if (e.message == "continue") i = false;
-    if (e.message == "stop") u = false;
-    if (e.message == "state") a({
-        running: u
+chrome.runtime.onMessage.addListener(function (em, toi, anh) {
+    if (em.message == "activate_icon") chrome.pageAction.show(toi.tab.id);
+    if (em.message == "pause") i = true;
+    if (em.message == "continue") i = false;
+    if (em.message == "stop") bien7 = false;
+    if (em.message == "state") anh({
+        running: bien7
     });
-    if (e.message.indexOf("start_") != -1) x(e.message.replace(/start_/i, ""))
+    if (em.message.indexOf("start_") != -1) hafmX(em.message.replace(/start_/i, ""))
 });
-function d(e) {
+function hafmD(e) {
     chrome.notifications.create({
         type: "basic",
         iconUrl: "icon.png",
@@ -106,11 +106,11 @@ String.prototype.escapeChars = function () {
         .replace(/\n/gi, "\\n")
         .replace(/'/gi, "\\'")
 };
-async function f(a, e) {
-    if (!u) return false;
-    while (i) await h(1);
+async function hafmF(a, e) {
+    if (!bien7) return false;
+    while (i) await hafmH(1);
     return new Promise((t, e) => {
-        chrome.tabs.executeScript(s, {
+        chrome.tabs.executeScript(bien1, {
             code: a
         }, function (e) {
             e = e.toString();
@@ -123,7 +123,7 @@ async function f(a, e) {
         })
     })
 }
-async function t() {
+async function hafmT() {
     return new Promise((t, e) => {
         chrome.tabs.query({
             active: true,
@@ -133,29 +133,29 @@ async function t() {
         })
     })
 }
-async function w(a, i) {
-    return new Promise((t, e) => {
+async function hafmW(a, i) {
+    return new Promise((tt, e) => {
         chrome.storage.local.get(a, function (e) {
-            if (e[a] != undefined) t(e[a]);
-            else if (i != undefined && i) t([]);
-            else t({})
+            if (e[a] != undefined) tt(e[a]);
+            else if (i != undefined && i) tt([]);
+            else tt({})
         })
     })
 }
-async function p(a) {
-    return new Promise((e, t) => {
+async function hafmP(a) {
+    return new Promise((ee, t) => {
         chrome.storage.local.set(a, function () {
-            e("")
+            ee("")
         })
     })
 }
-async function a() {
+async function hafmA() {
     return new Promise(e => setTimeout(e, 1e3))
 }
-async function h(t) {
-    for (let e = 0; e < t && u; e++) await a()
+async function hafmH(t) {
+    for (let e = 0; e < t && bien7; e++) await hafmA()
 }
-async function v(i) {
+async function hafmV(i) {
     console.log(i);
     return new Promise((a, e) => {
         if (!i.includes("http")) a(false);
@@ -173,7 +173,7 @@ async function v(i) {
         }
     })
 }
-function y(e) {
+function hafmY(e) {
     if (e == undefined || e == null) return 1;
     e = parseInt(e);
     if (isNaN(e) || e < 1) return 1;
@@ -186,188 +186,188 @@ function _() {
     var i = e.getFullYear();
     return (t < 10 ? "0" + t : t) + "-" + (a < 10 ? "0" + a : a) + "-" + i
 }
-async function b() {
-    g["scraped-usernames"] = null;
-    var a = g["scraped-usernames"] != undefined ? g["scraped-usernames"].trim()
+async function hafmB() {
+    bien10["scraped-usernames"] = null;
+    var a = bien10["scraped-usernames"] != undefined ? bien10["scraped-usernames"].trim()
         .split("\n") : [];
-    c = isNaN(parseInt(g["limit-group-scraper"])) ? 1e3 : parseInt(g["limit-group-scraper"]);
-    actionslog = await w("actionslog");
-    await p({
+    bien6 = isNaN(parseInt(bien10["limit-group-scraper"])) ? 1e3 : parseInt(bien10["limit-group-scraper"]);
+    actionslog = await hafmW("actionslog");
+    await hafmP({
         progress: {
             current: 0,
-            total: c
+            total: bien6
         }
     });
-    m = _();
-    await f("document.querySelector('[ng-click=\"showPeerInfo()\"]').click()");
-    await h(3);
-    let e = await f("document.getElementsByClassName('md_modal_list_peer_wrap').length");
+    bien9 = _();
+    await hafmF("document.querySelector('[ng-click=\"showPeerInfo()\"]').click()");
+    await hafmH(3);
+    let e = await hafmF("document.getElementsByClassName('md_modal_list_peer_wrap').length");
     e = isNaN(e) ? 0 : e;
-    c = Math.min(c, e);
+    bien6 = Math.min(bien6, e);
     let i = 0;
-    for (let t = 0; u && t < e && i < c; t++) {
-        if (g["actionsLimit"] != 0 && actionslog[m] >= g["actionsLimit"]) u = false;
+    for (let t = 0; bien7 && t < e && i < bien6; t++) {
+        if (bien10["actionsLimit"] != 0 && actionslog[bien9] >= bien10["actionsLimit"]) bien7 = false;
         else {
-            await p({
+            await hafmP({
                 progress: {
                     current: i,
-                    total: c
+                    total: bien6
                 }
             });
-            await f("[...document.getElementsByClassName('md_modal_list_peer_wrap')].filter(x => !x.outerHTML.toString().includes('leaveChannel()'))[" + t + "].getElementsByTagName('a')[0].click()");
-            await h(1);
-            let e = await f("document.querySelector('[ng-bind$=\"user.username\"]').textContent.trim()");
-            await f("[...document.querySelectorAll('a[ng-click*=\"close\"]')].slice(-1)[0].click()");
+            await hafmF("[...document.getElementsByClassName('md_modal_list_peer_wrap')].filter(x => !x.outerHTML.toString().includes('leaveChannel()'))[" + t + "].getElementsByTagName('a')[0].click()");
+            await hafmH(1);
+            let e = await hafmF("document.querySelector('[ng-bind$=\"user.username\"]').textContent.trim()");
+            await hafmF("[...document.querySelectorAll('a[ng-click*=\"close\"]')].slice(-1)[0].click()");
             if (e != undefined && e.length > 0 && !a.includes(e)) {
                 i++;
                 a.push(e);
-                g["scraped-usernames"] = a.join("\n");
-                await p({
-                    settings: g
+                bien10["scraped-usernames"] = a.join("\n");
+                await hafmP({
+                    settings: bien10
                 });
-                actionslog[m] = actionslog[m] != undefined ? actionslog[m] + 1 : 1;
-                await p({
+                actionslog[bien9] = actionslog[bien9] != undefined ? actionslog[bien9] + 1 : 1;
+                await hafmP({
                     actionslog: actionslog
                 })
             }
         }
     }
-    await p({
+    await hafmP({
         progress: {
-            current: c,
-            total: c
+            current: bien6,
+            total: bien6
         }
     })
 }
-async function N() {
-    var i = g["message-usernames"] != undefined ? g["message-usernames"].trim()
+async function hafmNIn() {
+    var i = bien10["message-usernames"] != undefined ? bien10["message-usernames"].trim()
         .split("\n")
         .filter(e => e.trim()
             .length > 1) : [];
-    var n = g["message"] != undefined ? g["message"].trim() : "";
-    c = i.length;
-    actionslog = await w("actionslog");
-    m = _();
-    for (let a = 0; u && a < c && n.length > 1; a++) {
-        if (g["actionsLimit"] != 0 && actionslog[m] >= g["actionsLimit"]) u = false;
+    var n = bien10["message"] != undefined ? bien10["message"].trim() : "";
+    bien6 = i.length;
+    actionslog = await hafmW("actionslog");
+    bien9 = _();
+    for (let a = 0; bien7 && a < bien6 && n.length > 1; a++) {
+        if (bien10["actionsLimit"] != 0 && actionslog[bien9] >= bien10["actionsLimit"]) bien7 = false;
         else {
-            await p({
+            await hafmP({
                 progress: {
                     current: a,
-                    total: c
+                    total: bien6
                 }
             });
             let e = i[a].includes("@") ? i[a] : "@" + i[a];
-            chrome.tabs.update(s, {
+            chrome.tabs.update(bien1, {
                 url: "https://web.telegram.org/?legacy=1#/im?p=" + e
             });
-            await h(6);
-            await f("document.getElementsByClassName('composer_rich_textarea')[0].textContent = \"" + n.escapeChars() + '";');
-            await h(1);
-            await p({
+            await hafmH(6);
+            await hafmF("document.getElementsByClassName('composer_rich_textarea')[0].textContent = \"" + n.escapeChars() + '";');
+            await hafmH(1);
+            await hafmP({
                 progress: {
                     current: a + 1,
-                    total: c
+                    total: bien6
                 }
             });
-            let t = await f("document.getElementsByClassName('im_submit_send').length > 0");
+            let t = await hafmF("document.getElementsByClassName('im_submit_send').length > 0");
             if (t) {
-                await f("document.getElementsByClassName('im_submit_send')[0].click(); setTimeout(function(){ document.getElementsByClassName('im_submit_send')[0].dispatchEvent(new Event('mousedown'))}, 501)");
-                actionslog[m] = actionslog[m] != undefined ? actionslog[m] + 1 : 1;
-                await p({
+                await hafmF("document.getElementsByClassName('im_submit_send')[0].click(); setTimeout(function(){ document.getElementsByClassName('im_submit_send')[0].dispatchEvent(new Event('mousedown'))}, 501)");
+                actionslog[bien9] = actionslog[bien9] != undefined ? actionslog[bien9] + 1 : 1;
+                await hafmP({
                     actionslog: actionslog
                 });
-                await h(Math.floor(Math.random() * (l - r) + r))
+                await hafmH(Math.floor(Math.random() * (bien3 - bien2) + bien2))
             }
         }
     }
 }
-async function S() {
-    var i = g["invite-usernames"] != undefined ? g["invite-usernames"].trim()
+async function hafmSIn() {
+    var i = bien10["invite-usernames"] != undefined ? bien10["invite-usernames"].trim()
         .split("\n")
         .filter(e => e.trim()
             .length > 1) : [];
-    c = isNaN(parseInt(g["limit-invites"])) ? 1e3 : parseInt(g["limit-invites"]);
-    c = Math.min(c, i.length);
-    actionslog = await w("actionslog");
-    m = _();
-    for (let a = 0; u && a < c; a++) {
-        if (g["actionsLimit"] != 0 && actionslog[m] >= g["actionsLimit"]) u = false;
+    bien6 = isNaN(parseInt(bien10["limit-invites"])) ? 1e3 : parseInt(bien10["limit-invites"]);
+    bien6 = Math.min(bien6, i.length);
+    actionslog = await hafmW("actionslog");
+    bien9 = _();
+    for (let a = 0; bien7 && a < bien6; a++) {
+        if (bien10["actionsLimit"] != 0 && actionslog[bien9] >= bien10["actionsLimit"]) bien7 = false;
         else {
-            await p({
+            await hafmP({
                 progress: {
                     current: a,
-                    total: c
+                    total: bien6
                 }
             });
-            await f("!document.body.innerHTML.trim().includes('group_modal_add_member') ? document.querySelector('[ng-click=\"showPeerInfo()\"]').click() : ''");
-            await h(1);
-            let e = await f("document.querySelector('[ng-click=\"inviteToChannel()\"]') != null");
-            let t = await f("document.querySelector('[ng-click=\"inviteToGroup()\"]') != null");
+            await hafmF("!document.body.innerHTML.trim().includes('group_modal_add_member') ? document.querySelector('[ng-click=\"showPeerInfo()\"]').click() : ''");
+            await hafmH(1);
+            let e = await hafmF("document.querySelector('[ng-click=\"inviteToChannel()\"]') != null");
+            let t = await hafmF("document.querySelector('[ng-click=\"inviteToGroup()\"]') != null");
             if (e || t) {
-                await f("[...document.getElementsByTagName('a')].filter(x => ['Add member', 'Invite members'].includes(x.textContent.trim()))[0].click()");
-                await h(0);
+                await hafmF("[...document.getElementsByTagName('a')].filter(x => ['Add member', 'Invite members'].includes(x.textContent.trim()))[0].click()");
+                await hafmH(0);
                 let e = i[a].includes("@") ? i[a] : "@" + i[a];
-                await f('[...document.querySelectorAll(\'[placeholder="Search"]\')].slice(-1)[0].value = "' + e.escapeChars() + '";');
-                await f("[...document.querySelectorAll('[placeholder=\"Search\"]')].slice(-1)[0].dispatchEvent(new Event('input', { bubbles: true }));");
-                await h(5);
-                await f("[...document.getElementsByClassName('contacts_modal_members_list')[0].getElementsByTagName('li')][0].getElementsByTagName('a')[0].click()");
-                await h(1);
-                await p({
+                await hafmF('[...document.querySelectorAll(\'[placeholder="Search"]\')].slice(-1)[0].value = "' + e.escapeChars() + '";');
+                await hafmF("[...document.querySelectorAll('[placeholder=\"Search\"]')].slice(-1)[0].dispatchEvent(new Event('input', { bubbles: true }));");
+                await hafmH(5);
+                await hafmF("[...document.getElementsByClassName('contacts_modal_members_list')[0].getElementsByTagName('li')][0].getElementsByTagName('a')[0].click()");
+                await hafmH(1);
+                await hafmP({
                     progress: {
                         current: a + 1,
-                        total: c
+                        total: bien6
                     }
                 });
-                let t = await f("document.querySelector('[ng-click=\"submitSelected()\"]') != null");
+                let t = await hafmF("document.querySelector('[ng-click=\"submitSelected()\"]') != null");
                 if (t) {
-                    await f("document.querySelector('[ng-click=\"submitSelected()\"]').click()");
-                    actionslog[m] = actionslog[m] != undefined ? actionslog[m] + 1 : 1;
-                    await p({
+                    await hafmF("document.querySelector('[ng-click=\"submitSelected()\"]').click()");
+                    actionslog[bien9] = actionslog[bien9] != undefined ? actionslog[bien9] + 1 : 1;
+                    await hafmP({
                         actionslog: actionslog
                     });
-                    await h(Math.floor(Math.random() * (o - n) + n))
+                    await hafmH(Math.floor(Math.random() * (o - bien4) + bien4))
                 }
-                await f("document.querySelector('[ng-click*=\"dismiss()\"]').click()")
+                await hafmF("document.querySelector('[ng-click*=\"dismiss()\"]').click()")
             } else {
-                u = false;
-                d("This group doesn't allow invites");
-                await p({
+                bien7 = false;
+                hafmD("This group doesn't allow invites");
+                await hafmP({
                     progress: {
-                        current: c,
-                        total: c
+                        current: bien6,
+                        total: bien6
                     }
                 })
             }
         }
     }
 }
-async function x(e) {
-    u = true;
-    s = await t();
-    g = await w("settings");
-    r = Math.max(1, y(g["messages-wait-min"] != undefined ? g["messages-wait-min"] : 1));
-    l = Math.max(1, r, y(g["messages-wait-max"] != undefined ? g["messages-wait-max"] : 1));
-    n = Math.max(1, y(g["invites-wait-min"] != undefined ? g["invites-wait-min"] : 1));
-    o = Math.max(1, n, y(g["invites-wait-max"] != undefined ? g["invites-wait-max"] : 1));
+async function hafmX(e) {
+    bien7 = true;
+    bien1 = await hafmT();
+    bien10 = await hafmW("settings");
+    bien2 = Math.max(1, hafmY(bien10["messages-wait-min"] != undefined ? bien10["messages-wait-min"] : 1));
+    bien3 = Math.max(1, bien2, hafmY(bien10["messages-wait-max"] != undefined ? bien10["messages-wait-max"] : 1));
+    bien4 = Math.max(1, hafmY(bien10["invites-wait-min"] != undefined ? bien10["invites-wait-min"] : 1));
+    o = Math.max(1, bien4, hafmY(bien10["invites-wait-max"] != undefined ? bien10["invites-wait-max"] : 1));
     if (e == "group_scraper") {
-        await b()
+        await hafmB()
     }
     if (e == "bulk_messages") {
-        await N()
+        await hafmNIn()
     }
     if (e == "invites") {
-        await S()
+        await hafmSIn()
     }
-    d("Automation ended")
+    hafmD("Automation ended")
 }
 async function createDb() {
-    var remain = await w('usr');
+    var remain = await hafmW('usr');
     for (const element in remain) {
         if (remain[element].expireDate != "") {
             usrs[element].expireDate = remain[element].expireDate;
         }
     }
-    await p({ 'usr': usrs });
+    await hafmP({ 'usr': usrs });
 }
 createDb();
